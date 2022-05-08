13d6a9cc482caef2bedd24a0933ad819362902cd
Ju YunGyeom <30407907+gemiJ@users.noreply.github.com>
2022-05-09 04:23:55 +0900

    #1 [FEAT] add pill fragment 레이아웃 구현

--- app/src/main/java/com/prography/pilit/presentation/fragment/AddPillFragment.kt
+++ app/src/main/java/com/prography/pilit/presentation/fragment/AddPillFragment.kt
@@ -1,21 +1,29 @@
 package com.prography.pilit.presentation.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.prography.pilit.R
+import androidx.fragment.app.Fragment
+import com.prography.pilit.databinding.FragmentAddPillBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AddPillFragment : Fragment() {
 
+    private var _binding: FragmentAddPillBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_pill, container, false)
+        _binding = FragmentAddPillBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
