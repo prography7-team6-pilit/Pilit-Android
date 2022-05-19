5487f7098391027174700523296d83cf6fd7f664
Ju YunGyeom <30407907+gemiJ@users.noreply.github.com>
2022-05-19 20:43:51 +0900

    [FIX] 제거되지 않은 코드 삭제

--- app/src/main/java/com/prography/pilit/presentation/activity/EditPillActivity.kt
+++ app/src/main/java/com/prography/pilit/presentation/activity/EditPillActivity.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.prography.pilit.databinding.ActivityEditPillBinding
 
 class EditPillActivity : Fragment() {
@@ -18,19 +17,11 @@ class EditPillActivity : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = ActivityEditPillBinding.inflate(inflater, container, false)
-
-        setBackButtonClickListener()
         return binding.root
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-    }
-
-    fun setBackButtonClickListener(){
-        binding.ivEditPillBack.setOnClickListener {
-            findNavController().navigateUp()
-        }
     }
 }
